147204e18940a2c95287bf82956ec60bdbc58938
Abdul Razzak <Abdul.Razzak@prhpl.com>
2026-06-24 22:23:48 +0530

    fix websocket for node 20

--- server/lib/supabase.js
+++ server/lib/supabase.js
@@ -1,11 +1,17 @@
 import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
+import { config } from 'dotenv';
+import ws from 'ws';
 
-dotenv.config();
+config({ path: '.env' });
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_ANON_KEY,
+  {
+    realtime: {
+      transport: ws,
+    },
+  }
 );
 
 export default supabase;
